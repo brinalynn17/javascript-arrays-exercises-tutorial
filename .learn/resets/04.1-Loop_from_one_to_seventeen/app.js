07755b34f596cf209663f71895694e2d38325735
Sabrina Hill <125325753+brinalynn17@users.noreply.github.com>
2023-10-09 23:26:45 +0000

    exercise updates

--- .learn/resets/04.1-Loop_from_one_to_seventeen/app.js
+++ .learn/resets/04.1-Loop_from_one_to_seventeen/app.js
@@ -1,5 +1,4 @@
 // Change the conditions of the for loop
-for(let number = 0; number <= 17; number++) {
+for(let number = 0; number < 10; number++) {
     // Print the number
 }  
-console.log(number);
--- .learn/resets/04.3-Add_items_to_array/app.js
+++ .learn/resets/04.3-Add_items_to_array/app.js
@@ -1,6 +1,5 @@
 let arr = [4,5,734,43,45];
 
 // Your code here, use the push() function and the 2 Math functions
-arr.push(25,30)
-arr.push(Math.floor(Math.Random()* 100))
+
 console.log(arr);
